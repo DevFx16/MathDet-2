473f2dcc0dd092d4f3fbf22e505531b16df6af0a
Fernando Araujo <fernandoaraujo081@gmail.com>
2018-12-28 22:23:55 -0500

    Determiante 3x3

--- MathDet/Src/Controllers/MatrizController.js
+++ MathDet/Src/Controllers/MatrizController.js
@@ -1,6 +1,7 @@
 export function Determinante(Valor, Matriz) {
-    console.log(Valor)
     if (Valor == 2) DetrminanteDos(Matriz);
+    else if (Valor == 3) DeterminanteTres(Matriz);
+    else DeterminanteCuatro(Matriz);
 }
 
 function DetrminanteDos(Matriz) {
@@ -8,4 +9,16 @@ function DetrminanteDos(Matriz) {
     let Mult2 = Matriz[0][1] * Matriz[1][0];
     let Det = Mult1 - Mult2;
     console.log(Det);
+}
+
+function DeterminanteTres(Matriz) {
+    let Co1 = Matriz[0][0] * (Matriz[1][1] * Matriz[2][2] - Matriz[1][2] * Matriz[2][1]);
+    let Co2 = Matriz[0][1] * (Matriz[1][0] * Matriz[2][2] - Matriz[1][2] * Matriz[2][0]);
+    let Co3 = Matriz[0][2] * (Matriz[1][0] * Matriz[2][1] - Matriz[2][0] * Matriz[1][1]);
+    let Det = Co1 - Co2 + Co3;
+    console.log(Det);
+}
+
+function DeterminanteCuatro(Matriz) {
+
 }
